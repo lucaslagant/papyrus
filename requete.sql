b37732a1a47a0aed8ddde278f9da7d4da65fdb20
LAGANT <lucaslagant@gmail.com>
2022-12-12 10:25:17 +0100

    final exercices

--- requete.sql
+++ requete.sql
@@ -70,3 +70,12 @@ SELECT numfou, qtecde * priuni * 0.20 FROM `ligcom`, `entcom` WHERE entcom.numco
 UPDATE `vente` SET prix1 = prix *0.04 AND prix2 = prix2 * 0.02 WHERE numfou = 9180;
 
 -- 2 - Dans la table vente, mettre à jour le prix2 des articles dont le prix2 est nul, en affectant la valeur du prix1.
+UPDATE `vente` SET prix2 = prix1 WHERE prix2 IS NULL;
+
+-- 3 - Mettre à jour le champ obscom, en renseignant ***** pour toutes les commandes dont le fournisseur a un indice de satisfaction inférieur à 5.
+UPDATE `entcom` JOIN `fournis` ON entcom.numfou = fournis.numfou SET obscom = '****' WHERE satisf < 5;
+
+-- 4 - Supprimer le produit "I110".
+DELETE FROM `produit` WHERE codart = 'I110';
+DELETE FROM `vente` WHERE codart = 'I110';
+DELETE FROM `ligcom` WHERE codart = 'I110';
